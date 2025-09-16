c095b9b53ec5bcc3912b3e8279863703fd33f2a9
matheus-Bugatti <campos.n@aluno.ifsp.edu.br>
2025-09-16 17:20:16 -0300

    add- middleware Logger build- verifier

---
+++ src/middleware/verifier.js
@@ -0,0 +1,3 @@
+export default verifier = (req, res, next) => {
+
+}
--- src/server.js
+++ src/server.js
@@ -2,10 +2,14 @@ import express from 'express'
 import cors from 'cors'
 import userRouter from './routers/userRouter.js'
 import productRouter from './routers/productRouter.js'
+import { logger } from './middleware/logger.js'
+import { verifier } from './middleware/verifier.js'
 
 const app = express()
 const port = 3000
 
+
+app.use(logger())
 app.use(cors())
 app.use(express.json())
 

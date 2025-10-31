8abcbec3edd8f380d350fe21d9c098cc1c2a199c
DevDaniel21 <danielglaucia2.007@gmail.com>
2025-10-31 16:17:08 -0300

    feat: excluido tabela imagem do schema.prisma

--- src/models/productModel.js
+++ src/models/productModel.js
@@ -3,30 +3,30 @@ import { PrismaClient } from '../generated/prisma/index.js';
 const prisma = new PrismaClient();
 
 export const create = async (product) => {
-    return prisma.product.create({
+    return prisma.produto.create({
         data: product,
     });
 };
 
 export const update = async (id, product) => {
-    return await prisma.product.update({
+    return await prisma.produto.update({
         data: product,
         where: { id },
     });
 };
 
 export const list = async () => {
-    return prisma.product.findMany();
+    return prisma.produto.findMany();
 };
 
 export const getById = async (id) => {
-    return await prisma.product.findUnique({
+    return await prisma.produto.findUnique({
         where: { id },
     });
 };
 
 export const remove = async (id) => {
-    return await prisma.product.delete({
+    return await prisma.produto.delete({
         where: { id },
     });
 };
